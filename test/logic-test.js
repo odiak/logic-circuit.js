787a91ef9777a0c31be347a0a9cd1a438e275fdd
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-10-01 03:57:50 +0900

    Add test code for Not node

--- test/logic-test.js
+++ test/logic-test.js
@@ -16,3 +16,17 @@ describe('And', () => {
     assert(and.outputPinAt(0).value === HIGH);
   });
 });
+
+describe('Not', () => {
+  it('should be low input is high', () => {
+    let not = new Not;
+    not.inputPinAt(0).setValue(HIGH);
+    assert(not.outputPinAt(0).value === LOW);
+  });
+
+  it('should be high input is low', () => {
+    let not = new Not;
+    not.inputPinAt(0).setValue(LOW);
+    assert(not.outputPinAt(0).value === HIGH);
+  });
+});
